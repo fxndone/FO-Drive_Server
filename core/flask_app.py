01f3fc10aba4e5641c4874ae905f03d41daeae90
fxndone <76475468+fxndone@users.noreply.github.com>
2022-07-12 20:12:25 +0200

    Update flask_app.py

--- core/flask_app.py
+++ core/flask_app.py
@@ -13,34 +13,31 @@ def home():
 
 @APP.route(PATH, methods=['GET', 'POST'])
 def file_sharing():
-    match request.method:
-        case "GET":
-            auth = request.headers.get("Authorization")
-            if auth is None:
-                print("No auth !")
-                abort(401)
-            if not do_header_match(auth):
-                print("Wrong auth !")
-                print(auth)
-                abort(401)
-            return send_file(os.path.join(DIRECTORY, "output.zip"), attachment_filename='output.zip')
-        case "POST":
-            auth = request.headers.get("Authorization")
-            if auth is None:
-                print("No auth post !")
-                abort(401)
-            if not  do_header_match(auth):
-                print("Wrong auth post !")
-                abort(401)
-            if 'file' not in request.files:
-                print("No file !")
-                abort(400)
-            file = request.files['file']
-            if file.filename == '':
-                abort(400)
-            if file:
-                file.save(os.path.join(DIRECTORY, "output.zip"))
-                return "Uploaded sucess !"
-            return "No file to upload !"
-        case _:
-            return "Wrong method !"
+    if request.method == "GET":
+        auth = request.headers.get("Authorization")
+        if auth is None:
+            print("No auth !")
+            abort(401)
+        if not do_header_match(auth):
+            print("Wrong auth !")
+            print(auth)
+            abort(401)
+        return send_file(os.path.join(DIRECTORY, "output.zip"), attachment_filename='output.zip')
+    elif request.method == "POST":
+        auth = request.headers.get("Authorization")
+        if auth is None:
+            print("No auth post !")
+            abort(401)
+        if not  do_header_match(auth):
+            print("Wrong auth post !")
+            abort(401)
+        if 'file' not in request.files:
+            print("No file !")
+            abort(400)
+        file = request.files['file']
+        if file.filename == '':
+            abort(400)
+        if file:
+            file.save(os.path.join(DIRECTORY, "output.zip"))
+            return "Uploaded sucess !"
+        return "No file to upload !"
